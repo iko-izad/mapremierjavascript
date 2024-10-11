74ae1ae366837d135f6718641dbe37fbbb4a48d2
Espace NUMERIQUE 4 <Espace4@numerique.lan>
2024-10-11 12:01:44 +0300

    fichier javacript

---
+++ condition.js
@@ -0,0 +1,36 @@
+// LES CONDITIONS
+/*
+Si A existe,
+alors B s'affiche.
+*/
+
+// ON utilise le mot-clé if() pour crér une condition
+
+let note = 12
+
+
+// Je veux vérifier si la personne est majeure
+if(note == 12) {
+    alert("Assez bien");
+}
+
+// Je veux vérifier si la personne est mineure
+
+
+if(note == 20) {
+   "trés bien";
+
+}
+/* Si la note est 5, on affiche insuffisant*/
+if(note == 5) {
+   "insuffisant";
+
+}
+
+/* Si la note est 15, on affiche bien */
+if(note == 15) {
+    "bien";
+
+}
+
+
